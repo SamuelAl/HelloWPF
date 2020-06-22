20b8f782f21d926d6609df9707cb0fe0c862aa2b
Samuel Alarco Cantos <ALARCOCS@tcd.ie>
2020-06-22 17:06:40 +0100

    Use of application xaml and ceo-in-behind to load window

--- HelloWPF/App.xaml.cs
+++ HelloWPF/App.xaml.cs
@@ -13,5 +13,14 @@ namespace HelloWPF
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // Create the startup window
+            MainWindow window = new MainWindow();
+            // Do stuff here, e.g. to the window
+            window.Title = "New Title";
+            // Show the window
+            window.Show();
+        }
     }
 }
